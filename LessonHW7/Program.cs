2fd4ca6ea2f0e9a986584a6d7b0833a90627ce38
Dell <Dell@DESKTOP-B6FMJUN>
2022-03-17 14:50:40 +0200

    Добавьте файлы проекта.

---
+++ LessonHW7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LessonHW7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                //Задача: запросить у пользователя число, факториал которого необходимо вычислить и произвести вычисление. 
+                //Затем вывести результат вычисления. Восклицательный знак запрашивать не надо, 
+                //кроме того, в C# такой операции нет. Для вычисления факториала надо производить перемножение.
+
+
+
+            Console.WriteLine("Введите число факториал которого вы хотите вычислить:");
+
+            int input = Factorial(int.Parse(Console.ReadLine()));
+
+            int Factorial(int n)
+            {
+                if (n == 1) return 1;
+                return n * Factorial(n - 1);    
+            }
+
+            Console.WriteLine($"Факториал вашего числа равен : {input}");
+            Console.ReadLine();
+        }
+    }
+}
